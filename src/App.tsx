ec5b0fc503455784bc92445309b66fc35cece652
ivenkatesh14 <64054299+ivenkatesh14@users.noreply.github.com>
2023-04-18 20:49:52 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -224,7 +224,7 @@ function App() {
   return (
     <div className="py-8 max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div className="flex w-80 mx-auto items-center mb-8">
-        <h1 className="text-xl grow font-bold">Gita Wordle</h1>
+        <h1 className="text-xl grow font-bold">Geeta Wordle</h1>
         <InformationCircleIcon
           className="mx-2 h-6 w-6 cursor-pointer"
           onClick={() => onInfoModal()}
